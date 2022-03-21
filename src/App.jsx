33b7ab2331d0eb7b0670e0bc012f13879bf63d7d
jswxxkf <xuekaifeng012@gmail.com>
2022-03-21 11:09:21 +0800

    chore: 路由模式由hash改为history

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { memo, Suspense } from "react";
-import { Provider } from "react-redux";
 // router & service & store
 import { renderRoutes } from "react-router-config";
 import routes from "./router";
 import store from "./store";
-import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 // sub cpn
 import KFAppHeader from "@/components/app-header";
 import KFAppFooter from "@/components/app-footer";
@@ -14,12 +14,12 @@ import Fallback from "./components/fallback";
 export default memo(function App() {
   return (
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <KFAppHeader />
         <Suspense fallback={<Fallback />}>{renderRoutes(routes)}</Suspense>
         <KFAppFooter />
         <KFAppPlayerBar />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   );
 });
